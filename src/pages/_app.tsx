5b760d4522f3cd7860d69410322e9c53b545e274
Guilherme Lino <guilhermelinosp@gmail.com>
2022-12-21 07:08:03 -0300

    feat: init new project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import { AuthProvider } from '../contexts/AuthContext'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
